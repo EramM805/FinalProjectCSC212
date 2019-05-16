de64ee4f80a29d2ade201c42af581a179dded6a9
Faez Farhad <f.faezgtm@gmail.com>
2019-05-16 16:29:34 -0400

    changes

--- blackjack.cxx
+++ blackjack.cxx
@@ -1,22 +1,57 @@
-#include <cstdlib>     // Provides rand and srand
+#include <cstdlib>     // Provides EXIT_SUCCESS
 #include <iostream>    // Provides cout
 #include <string>      // Provides string class
 #include "table.h"   // Provides the header file
 using namespace std;
 using namespace main_savitch_12A;
-/*/ Game rules: 
-Aces,2,3,4,5,6,7,8,9 are 4 cards
-10 is 16 cards because kings, queens and jack count as 10
-/*/
-//Prototypes used for functions in this game program:
-void setup();
 
-void shuffle(int card[], int n){
-    srand(time(0));
-    for (int i =0; i < n; i++){
-        int r = i + (rand() % (52-i));
-        swap(card[i], card[r]);
-    }
+//Prototypes used for functions in this game program:
+
+int hashkey(const card& c) {
+        return c.key; 
+    } 
+void setup(){
+    table<card, 13, hashkey> my_table;
+    card data[13];
+    data[0].key = 0;
+    data[0].value = 1;
+    data[1].key = 1;
+    data[1].value = 2;
+    data[2].key = 2;
+    data[2].value = 3;
+    data[3].key = 3;
+    data[3].value = 4;
+    data[4].key = 4;
+    data[4].value = 5;
+    data[5].key = 5;
+    data[5].value = 6;
+    data[6].key = 6;
+    data[6].value = 7;
+    data[7].key = 7;
+    data[7].value = 8;
+    data[8].key = 8;
+    data[8].value = 9;
+    data[9].key = 9;
+    data[9].value = 10;
+    data[10].key = 10;
+    data[10].value = 10;
+    data[11].key = 11;
+    data[11].value = 10;
+
+     my_table.insert(data[0]);
+     my_table.insert(data[1]);
+     my_table.insert(data[2]);
+     my_table.insert(data[3]);
+     my_table.insert(data[4]);
+     my_table.insert(data[5]);
+     my_table.insert(data[6]);
+     my_table.insert(data[7]);
+     my_table.insert(data[8]);
+     my_table.insert(data[9]);
+     my_table.insert(data[10]);
+      my_table.insert(data[11]);
+       my_table.insert(data[12]);
+    //std::cout<<my_table.is_present(12)<<std::endl;
 }
 
 void instruct( ){
@@ -26,6 +61,14 @@ void instruct( ){
     cout << "How many players are playing?" << endl;
     cin >> players;
 }
+void shuffle(int card[], int n){
+    srand(time(0));
+    for (int i =0; i < n; i++){
+        int r = i + (rand() % (52-i));
+        swap(card[i], card[r]);
+    }
+}
+
 
 void exitGame(){
     int exitGame = 1;
@@ -37,16 +80,15 @@ void exitGame(){
     return;
 }
 
-
 int main() {
-    instruct ( );
-
-    int a[] = {0, 1, 2, 3, 4 ,4 ,5 ,6 ,7 ,8, 9 , 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46 ,47, 49, 50, 51};
+    cout << "Shuffling the deck.." << endl;
+    int a[] = {0, 1, 2, 3, 4, 5, 6 ,7 ,8, 9 , 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46 ,47, 49, 50, 51};
     shuffle (a, 52);
     for (int i=0; i<52; i++)
     cout << a[i] << " "; 
-    cout << "The deck has been shuffled" << endl; 
     cout << endl;
-
+    cout << "The deck has been shuffled" << endl; 
+    setup();
+    //instruct ( );
+    
     return 0;
-}
---
+++ build/CMakeFiles/table1.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libtable1.a"
+)
