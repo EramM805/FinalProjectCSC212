3a1386d53b3b700b4e2fb3830b28d5963f68e93c
Faez Farhad <f.faezgtm@gmail.com>
2019-05-16 15:23:00 -0400

    added shuffle, added how many plays are playing, game rules

---
+++ build/CMakeFiles/table1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/table1.dir/blackjack.cpp.o"
+  "libtable1.pdb"
+  "libtable1.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/table1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
