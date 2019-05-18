37fdb9e24f3012f46494776059274e49bea6eef9
Faez Farhad <f.faezgtm@gmail.com>
2019-05-17 20:53:09 -0400

    added players

--- blackjack.cxx
+++ blackjack.cxx
@@ -8,20 +8,20 @@ using namespace main_savitch_12A;
 //Prototypes used for functions in this game program:
 
 int hashkey(const card& c) {
-        return c.key; 
+    return c.key;
 }
 bool removetail(card c[], int key){
     bool removed = false;
     std::cout<<c[key].value.length(c[key].value[0]);
-        if(c[key].value.length(c[key].value[0])){
-            removed = c[key].value.remove_tail(c[key].value.length(c[key].value[0]));
-        }
-        else{
-             std::cout<<hashkey(c[key])<<std::endl;
-            //throw std::out_of_range ("Key does not exist");
-        }
+    if(c[key].value.length(c[key].value[0])){
+        removed = c[key].value.remove_tail(c[key].value.length(c[key].value[0]));
+    }
+    else{
+        std::cout<<hashkey(c[key])<<std::endl;
+        //throw std::out_of_range ("Key does not exist");
+    }
     return removed;
-} 
+}
 int sum(card c[], int length, int key){
     int total = 0;
     for(int i = 0; i < length; i++){
@@ -32,7 +32,7 @@ int sum(card c[], int length, int key){
 void setup(){
     //create linked list
     //create table
-    //add linked list to table 
+    //add linked list to table
     table<card, 11, hashkey> my_table;
     int key_local;
     card data[10];
@@ -50,14 +50,40 @@ void setup(){
     std::cout<<data[1].value.length(data[1].value[0]);
     //std::cout<<data[2].value.length(data[2].value[0])<<std::endl;
     //std::cout<<data[2].value[15]->data()<<std::endl;
-
+    
 }
+
+void addPlayers(){
+    //hash table for amount of players
+    //key is the player number and value is linked list of cards
+    table<card, 7, hashkey> my_table;
+    int players=0;
+    card data[7];
+    data[1].key = players;
+    for(players = 1; players <= 7; players++){
+        for(int i = 0; i < 3; i++){
+            data[players-1].value.append(players);
+        }
+    }
+    std::cout << "Player 1 was assigned card: " <<sum(data, data[1].value.length(data[0].value[0]), 1)<< std::endl;
+    std::cout << "Player 2 was assigned card: " <<sum(data, data[2].value.length(data[0].value[0]), 2)<< std::endl;
+    std::cout << "Player 3 was assigned card: " <<sum(data, data[3].value.length(data[0].value[0]), 3)<< std::endl;
+    std::cout << "Player 4 was assigned card: " <<sum(data, data[4].value.length(data[0].value[0]), 4)<< std::endl;
+    std::cout << "Player 5 was assigned card: " <<sum(data, data[5].value.length(data[0].value[0]), 5)<< std::endl;
+    std::cout << "Player 6 was assigned card: " <<sum(data, data[6].value.length(data[0].value[0]), 6)<< std::endl;
+}
+
 
 void instruct( ){
     cout << "Welcome to Blackjack Game program!" << endl;
     int players;
-    cout << "How many players are playing?" << endl;
-    cin >> players;
+    do {
+        cout << "How many players are playing?" << endl;
+        cin >> players;
+    } while (players == 1);
+    addPlayers();
+    return;
+    
 }
 
 void shuffle(int card[], int n){
@@ -72,8 +98,8 @@ void shuffle(int card[], int n){
 void exitGame(){
     int exitGame = 1;
     do {
-    cout << "Would you like to play again? (1 = Yes, 2 = No)" << endl;
-    cin >> exitGame;
+        cout << "Would you like to play again? (1 = Yes, 2 = No)" << endl;
+        cin >> exitGame;
     } while (exitGame == 1);
     cout << "Thanks for playing!" << endl;
     return;
@@ -84,12 +110,15 @@ int main() {
     // int a[] = {0, 1, 2, 3, 4, 5, 6 ,7 ,8, 9 , 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46 ,47, 49, 50, 51};
     // shuffle (a, 52);
     // for (int i=0; i<52; i++)
-    // cout << a[i] << " "; 
+    // cout << a[i] << " ";
     // cout << endl;
-    // cout << "The deck has been shuffled" << endl;  
+    // cout << "The deck has been shuffled" << endl;
     setup();
-    //instruct ( );
+    instruct ( );
+    //addPlayers();
+    exitGame();
     
     
     return 0;
 }
+
