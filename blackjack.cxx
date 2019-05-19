77823f71f5d06210dc38492a3737f2e759c4243d
Faez Farhad <f.faezgtm@gmail.com>
2019-05-19 03:14:02 -0400

    final pushes, organized the code

--- blackjack.cxx
+++ blackjack.cxx
@@ -6,7 +6,6 @@ using namespace std;
 using namespace main_savitch_12A;
 
 //Prototypes used for functions in this game program:
-
 int hashkey(const card& c) {
     return c.key;
 }
@@ -17,10 +16,25 @@ bool removetail(card c[], int key){
     }
     else{
         return removed;
-        //std::cout<<hashkey(c[key])<<std::endl;
-        //throw std::out_of_range ("Key does not exist");
     }
     return removed;
+}
+void setup(card data[]){
+    //create linked list
+    //create table
+    //add linked list to table
+    //table<card, 11, hashkey> my_table;
+    int key_local=0;
+    /*card data[10];*/
+    data[0].key = key_local;
+    for(key_local = 1; key_local < 10; key_local++){
+        for(int i = 0; i < 4; i++){
+            data[key_local-1].value.append(key_local);
+        }
+    }
+    for(int i = 0; i < 16; i++){
+        data[key_local-1].value.append(key_local);
+    }
 }
 int sum(card c[], int length, int key){
     int total = 0;
@@ -43,37 +57,6 @@ int sum(card c[], int length, int key){
     }
     return total;
 }
-void setup(card data[]){
-    //create linked list
-    //create table
-    //add linked list to table
-    //table<card, 11, hashkey> my_table;
-    int key_local=0;
-    /*card data[10];*/
-    data[0].key = key_local;
-    for(key_local = 1; key_local < 10; key_local++){
-        for(int i = 0; i < 4; i++){
-            data[key_local-1].value.append(key_local);
-        }
-    }
-    for(int i = 0; i < 16; i++){
-        data[key_local-1].value.append(key_local);
-    }
-    // std::cout<<sum(data, data[1].value.length(data[1].value[0]), 1)<<std::endl;
-    // std::cout << removetail(data, 1);
-    // std::cout<<data[1].value.length(data[1].value[0]);
-    //std::cout<<data[2].value.length(data[2].value[0])<<std::endl;
-    //std::cout<<data[2].value[15]->data()<<std::endl;
-    
-}
-//destructing players, card and deck when we replaying
- void destructor(card playerss[], card dealer[], card data[], card scores[]){
-     delete playerss;
-     delete dealer;
-     delete data;
-     delete scores;
-}
-
 void initial_distribute(card players[], int numofplayers, card c[]){
     for(int i = 0; i < numofplayers; i++){
         while(players[i].value.length(players[i].value[0]) != 2){
@@ -98,25 +81,15 @@ void distribute(card player[], int key, card c[]){
         }
     }
 }
-/*void addPlayers(card players[]){
-    //hash table for amount of players
-    //key is the player number and value is linked list of cards
-    table<card, 7, hashkey> player_table;
-    int numplayers=0;
-    card data[7];
-    data[1].key = numplayers;
-    for(numplayers = 1; numplayers < key; numplayers++){
-        for(int i = 0; i < 3; i++){
-            data[numplayers-1].value.append(numplayers);
-        }
-    }
-    std::cout << "Player 1 was assigned card: " <<sum(data, data[1].value.length(data[1].value[0]), 1)<< std::endl;
-    std::cout << "Player 2 was assigned card: " <<sum(data, data[2].value.length(data[2].value[1]), 2)<< std::endl;
-    std::cout << "Player 3 was assigned card: " <<sum(data, data[3].value.length(data[3].value[0]), 3)<< std::endl;
-    std::cout << "Player 4 was assigned card: " <<sum(data, data[4].value.length(data[4].value[1]), 4)<< std::endl;
-    std::cout << "Player 5 was assigned card: " <<sum(data, data[5].value.length(data[5].value[0]), 5)<< std::endl;
-    std::cout << "Player 6 was assigned card: " <<sum(data, data[6].value.length(data[6].value[1]), 6)<< std::endl;
-}*/
+
+//destructing players, card and deck when we replaying
+ void destructor(card playerss[], card dealer[], card data[], card scores[]){
+     delete playerss;
+     delete dealer;
+     delete data;
+     delete scores;
+}
+
 void addDealer(card dealer[], int key, card c[]){
     table<card, 1, hashkey> dealer_table;
     int numdealer=1;
@@ -140,21 +113,6 @@ void init_distribute_dealer(card dealer[], card c[]){
         }
 }
 
-void scoreboard(card score[], int sum, int key){
-    if(sum > 21){
-        score[key].value.append(0); 
-    }
-    else{
-        score[key].value.append(sum);
-    }
-}
-
-void show_player_hand(card player[], int key){
-    for(unsigned int j = 0; j < player[key].value.length(player[key].value[0]); j++){
-        std::cout << " " << player[key].value[j]->data();
-    }
-}
-
 void decision(card player[], int key, card c[]){
     int decision;
     do {
@@ -166,7 +124,7 @@ void decision(card player[], int key, card c[]){
             cout << "Total: " << sum(player, player[key].value.length(player[key].value[0]), key) << endl;
             }
             else{
-                std::cout << "You have BUSTED!" << endl;
+                std::cout << "Player has been BUSTED!" << endl;
                 break;
             }
         }
@@ -184,11 +142,25 @@ void decision_dealer(card dealer[], int key, card c[]){
             cout << "Total: " << sum(dealer, dealer[key].value.length(dealer[key].value[0]), key) << endl;
         }
         else{
-            cout << "Dealer has BUSTED!" << endl;
+            cout << "Dealer has been BUSTED!" << endl;
             break;
         }
         }
     } while (decision == 1);
+}
+void scoreboard(card score[], int sum, int key){
+    if(sum > 21){
+        score[key].value.append(0);
+    }
+    else{
+        score[key].value.append(sum);
+    }
+}
+
+void show_player_hand(card player[], int key){
+    for(unsigned int j = 0; j < player[key].value.length(player[key].value[0]); j++){
+        std::cout << " " << player[key].value[j]->data();
+    }
 }
 
 void instruct( ){
@@ -240,7 +212,7 @@ void instruct( ){
 
     //Reveals players' hands
     for(int z = 0; z < playerss; z++){
-        std::cout << "Player " << z+1 << ":";
+        std::cout << "Player " << z+1 << ": ";
         show_player_hand(players, z);
         std::cout << "\t Total: " << sum(players, players[z].value.length(players[z].value[0]), z) << endl;
 
@@ -252,37 +224,35 @@ void instruct( ){
     }
 
     //Dealer's hand is revealed
-    std::cout << "Dealer has:";
+    std::cout << "Dealer has: ";
     show_player_hand(dealer, 1);
-    std::cout<< "\t Total:" << sum(dealer, dealer[1].value.length(dealer[1].value[0]), 1) << endl;
+    std::cout<< "\t Total: " << sum(dealer, dealer[1].value.length(dealer[1].value[0]), 1) << endl;
 
     //If Dealer has 16 or below
     if(sum(dealer, dealer[1].value.length(dealer[1].value[0]), 1) <= 16){
         std::cout << "The Dealer has a total sum of 16 or below!" << endl;
         std::cout << "The Dealer must draw again!" << endl;
         distribute(dealer, 1, data);
-        std::cout << "Dealer NOW HAS:";
+        std::cout << "Dealer NOW HAS: ";
         show_player_hand(dealer, 1);
         std::cout << endl;
         if(sum(dealer, dealer[1].value.length(dealer[1].value[0]), 1) < 21){
-            std::cout << "New Total:" << sum(dealer, dealer[1].value.length(dealer[1].value[0]), 1) << endl;
+            std::cout << "New Total: " << sum(dealer, dealer[1].value.length(dealer[1].value[0]), 1) << endl;
             //Dealer will decide to hit or stand
             decision_dealer(dealer, 1, data);
         }
         else if(sum(dealer, dealer[1].value.length(dealer[1].value[0]), 1) == 21){
-            std::cout << "New Total:" << sum(dealer, dealer[1].value.length(dealer[1].value[0]), 1) << endl;
+            std::cout << "New Total: " << sum(dealer, dealer[1].value.length(dealer[1].value[0]), 1) << endl;
             cout << "The dealer has won." << endl;
         }
         else{
-            std::cout << "The Dealer has busted!";
+            std::cout << "The Dealer has been busted!";
 
         }
     }
     else if(sum(dealer, dealer[1].value.length(dealer[1].value[0]), 1) == 21){
         cout << "The dealer has already won." << endl;
     }
-
-
 
     //Generate Scoreboard
     for(int q = 0; q < playerss; q++){
@@ -295,7 +265,7 @@ void instruct( ){
             cout << "Player " << r+1 << ": " << "BUSTED!" << endl;
         }
         else{
-            cout << "Player " << r+1 << ":" << scores[r].value[0]->data();
+            cout << "Player " << r+1 << ": " << scores[r].value[0]->data();
             if(scores[r].value[0]->data() > sum(dealer, dealer[1].value.length(dealer[1].value[0]), 1)){
                 cout << "\t WINNER!" << endl;
             }
@@ -309,12 +279,7 @@ void instruct( ){
     return;
     
 }
-
-
 int main() {
-    //instruct ( );
-    //addPlayers();
-    {
         bool play_again = true;
         while (play_again)
         {
@@ -328,10 +293,6 @@ int main() {
                 std::cout << "Thanks for playing Blackjack!" << endl;
             }
         }
-        return 0;
-    }
-    
-    
     return 0;
 }
 
